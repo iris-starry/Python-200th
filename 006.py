b04e2d1f3ec722723f83b5c37dc93c1ab6878803
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-09 23:47:13 +0900

    자료형 개념 배우기

---
+++ 006.py
@@ -0,0 +1,8 @@
+int_data = 1                       #정수 선언
+float_data = 3.14                   #실수 선언
+complex_data = 1+5j                 #복소수 선언
+str_data1 = 'I love Python'         #문자열 선언(영문)
+str_data2 = "반갑습니다."           #문자열 선언(한글)
+list_data = [1,2,3]                 #리스트 선언
+tuple_data = (1,2,3)                #튜플 선언
+dict_data = {0:'False',1:'True'}    #사전 선언
