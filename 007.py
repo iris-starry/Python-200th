a5f4a33508b57df44365d205555c8489a0d5e19c
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-09 23:47:35 +0900

    자료형 출력 개념 배우기(print)

---
+++ 007.py
@@ -0,0 +1,18 @@
+a = 200
+msg = 'I love Python'
+list_data = ['a','b','c']
+dict_data = {'a':97,'b':98}
+print(a)
+print(msg)
+print(list_data)
+print(list_data[0])
+print(dict_data)
+print(dict_data['a'])
+
+
+#print()는 기본적으로 인자로 입력된 값을 화면에 출력한 후 줄바꿈을 합니다.
+"""만약 줄바꿈 문자 '/n'을 추가하지 않으려면
+라인 끝에서 다음과 같이 print()의 두 번째 인자로 end=""를 지정하면 됩니다."""
+
+print('#',end="")
+print('#')
