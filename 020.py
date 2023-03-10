5ea2d1cbb07a9271ab46a238c6a7c467ac5e98ce
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-10 21:16:49 +0900

    사칙 연산자 이해하기 (+,-,*,/,**)

---
+++ 020.py
@@ -0,0 +1,10 @@
+a = 2
+b = 4
+ret1 = a+b          #ret1 = 2+4=6
+ret2 = a-b          #ret2 = 2-4=-2
+ret3 = a*b          #ret3 = 2*4=8
+ret4 = a/b          #ret4 = 2/4=8
+ret5 = a**b         #ret5 = 2**4=16
+ret6 = a+a*b/a      #ret6 = 2+2*4/2=6
+ret7 = (a+b)*(a-b)  #ret7 = (2+4)*(2-4)
+ret8 = a*b**a       #ret8 = 2*4**2=32
