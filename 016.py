52602c6a578f457362824d7679f5c22b3bf81105
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-10 21:15:25 +0900

    정수형 자료 이해하기

---
+++ 016.py
@@ -0,0 +1,11 @@
+int_data = 10
+bin_data = 0b10
+oct_data = 0o10
+hex_data = 0x10
+long_data = 123456789023456789
+print(int_data)
+print(bin_data)
+print(oct_data)
+print(hex_data)
+print(long_data)
+
