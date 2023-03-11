7f9921511f718d7f3fc0e86622f29cffb9df0319
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-11 23:12:26 +0900

    시퀀스 자료 슬라이싱 이해하기

---
+++ 028.py
@@ -0,0 +1,8 @@
+strdata = 'Time is money!!'
+print(strdata[1:5])   #'ime'가 출력됨
+print(strdata[:7])    #'Time is'가 출력됨
+print(strdata[9:])    #'oney!!'가 출력됨
+print(strdata[:-3])   #'Time is mone'이 출력됨
+print(strdata[-3:])   #'y!!'이 출력됨
+print(strdata[:])     #'Time is money!!'가 출력됨
+print(strdata[::2])   #'Tm smny!'가 출력
