858c2071517663c2f45f731438eeaa7bb32a8eb4
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-11 23:10:48 +0900

    논리 연산자 이해하기 (and,or,not)

---
+++ 024.py
@@ -0,0 +1,7 @@
+bool1 = True;bool2 = False;bool3 = True;bool4 = False
+print(bool1 and bool2)   #False가 출력됨
+print(bool1 and bool3)   #True가 출력됨
+print(bool2 or bool3)    #True가 출력됨
+print(bool2 or bool4)    #False가 출력됨
+print(not bool1)         #False가 출력됨
+print(not bool2)         #True가 출력
