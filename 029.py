cee7bcb1e8a79c501c05b581a28a1b0c8a57d7db
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-11 23:12:47 +0900

    시퀀스 자료 연결 이해하기(+)

---
+++ 029.py
@@ -0,0 +1,5 @@
+strdata1 = 'I love '; strdata2 = 'Python'; strdata3 = 'you'
+listdata1 = [1,2,3]; listdata2 = [4,5,6]
+print(strdata1 + strdata2)     #'I love Python'이 출력됨
+print(strdata1 + strdata3)     #'I love you'가 출력됨
+print(listdata1 + listdata2)   #[1,2,3,4,5,6]이 출력
