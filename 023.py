f2f9e5a6263671d4aa33fabd7f0eab2735751410
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-11 23:10:10 +0900

    관계 연산자 이해하기 (==,!=,<,<=,>,>=)

---
+++ 023.py
@@ -0,0 +1,7 @@
+x = 1; y = 2
+str1 = 'abc'; str2 = 'python'
+print(x == y)              #False가 출력됨
+print(x != y)              #True가 출력됨
+print(str1 == str2)        #False가 출력됨
+print(str2 == 'python')    #True가 출력됨
+print(str1 < str2)         #True가 출력
