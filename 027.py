355abd8bd7c42a8b056d1ec75c12072dd66031c9
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-11 23:12:05 +0900

    시퀸스 자료 인덱싱 이해하기

---
+++ 027.py
@@ -0,0 +1,7 @@
+strdata = 'Time is money!!'
+listdata = [1,2,[1,2,3]]
+print(strdata[5])       #'i'가 출력됨
+print(strdata[-2])      #'!'가 출력됨
+print(listdata[0])      #1이 출력됨
+print(listdata[-1])     #[1,2,3]이 출력됨
+print(listdata[2][-1])  #3이 출력
