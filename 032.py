292246a9f37182e700157b3c9e7f482f9ea9d001
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-12 21:55:20 +0900

    멤버체크 이해하기(in)

---
+++ 032.py
@@ -0,0 +1,8 @@
+listdata = [1,2,3,4]
+ret1 = 5 in listdata       #False
+ret2 = 4 in listdata       #True
+print(ret1); print(ret2)   
+strdata = 'abcde'
+ret3 = 'c' in strdata      #True
+ret4 = '1' in strdata      #False
+print(ret3); print(ret4)
