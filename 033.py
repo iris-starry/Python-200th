cd482733098484eb8f719a8d3623f063def9f60b
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-12 21:56:06 +0900

    문자열 이해하기

---
+++ 033.py
@@ -0,0 +1,8 @@
+strdata1 = '나는 파이썬 프로그래머다'         #문자열 선언 방법1
+strdata2 = "You are a programmer"             #문자열 선언 방법2
+strdata3 = """I love 
+    pyton. You love
+python too!
+"""
+strdata4 = "My son's name is John"            #" "안에서'사용하기
+strdata5 = '문자열 "abc"의 길이는 3입니다.'   #' '안에서" 사용하기
