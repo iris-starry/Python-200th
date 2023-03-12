646ac1e53f3ea2455af894b9fa142d6b1eb74162
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-12 21:54:42 +0900

    시퀀스 자료 크기 이해하기(len)

---
+++ 031.py
@@ -0,0 +1,7 @@
+strdata1 = 'I love python'
+strdata2 = '나는 파이썬을 사랑합니다'
+listdata = ['a','b','c', strdata1, strdata2]
+print(len(strdata1))      #13이 출력됨
+print(len(strdata2))      #13이 출력됨
+print(len(listdata))      #5가 출력됨
+print(len(listdata[3]))   #13이 출력
