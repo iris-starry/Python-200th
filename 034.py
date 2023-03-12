87b9614a15a9462d47c8bc0118e5d0e3fd2c5233
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-03-12 21:56:49 +0900

    문자열 포맷팅 이해하기

---
+++ 034.py
@@ -0,0 +1,6 @@
+txt1 = '자바'; txt2 = '파이썬'
+num1 = 5; num2 =10
+print('나는 %s보다 %s에 더 익숙합니다.' %(txt1, txt2))
+print('%s은 %s보다 %d배 더 쉽습니다.' %(txt2, txt1, num1))
+print('%d + %d = %d' %(num1,num2,num1+num2))
+print('작년 세계 경제 성장률은 전년에 비해 %d%% 포인트 증가했다.'%num1)
