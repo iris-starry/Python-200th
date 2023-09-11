95ce92609886768f12e249d3fd2d33b40e0b293f
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-09-11 20:30:44 +0900

    자료형 확인하기(type)

---
+++ 061.py
@@ -0,0 +1,13 @@
+numdata = 57
+strdata = '파이썬'
+listdata = [1,2,3]
+dictdata = {'a':1,'b':2}
+
+def func():
+    print('안녕하세요.')
+
+print(type(numdata))
+print(type(strdata))
+print(type(listdata))
+print(type(dictdata))
+print(type(func))
