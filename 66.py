735907aa7adbf79019ed32726defbc495957090f
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2023-09-13 04:46:35 +0900

    2진수, 16진수를 10진수로 변환하기(int)

---
+++ 66.py
@@ -0,0 +1,10 @@
+bnum = 0b11110000; bstr = '0b11110000'
+onum = 0o360; ostr = '0o360'
+hnum = 0xf0; hstr = '0xf0'
+b1 = int(bnum); b2 = int(bstr,2)  
+o1 = int(onum); o2 = int(ostr,8) 
+h1 = int(hnum); h2 = int(hstr,16)
+print(b1); print(b2)
+print(o1); print(o2)
+print(h1); print(h2)
+
