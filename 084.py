23c6b80d92ddf8c7dea9c93edf9a2b3e87006df6
sunhee kim <106311884+iris-starry@users.noreply.github.com>
2024-01-02 20:35:53 +0900

    문자열이 알파벳인지 검사하기(isalpha)

---
+++ 084.py
@@ -0,0 +1,12 @@
+txt1 = 'A'
+txt2 = '안녕'
+txt3 = 'Warcraft Three'
+txt4 = '3PO'
+ret1 = txt1.isalpha()
+ret2 = txt2.isalpha()
+ret3 = txt3.isalpha()
+ret4 = txt4.isalpha()
+print(ret1)
+print(ret2)
+print(ret3)
+print(ret4)
