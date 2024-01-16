5d108b01d6ac301ef5f5c5f5cbf346de91fae2fc
iris-starry <iris_starry_@naver.com>
2024-01-17 01:11:06 +0900

    문자열에 있는 문자(열) 개수 구하기(count)

---
+++ 091.py
@@ -0,0 +1,7 @@
+txt = 'A lot of things occur each day, every day.'
+word_count1 = txt.count('o')
+word_count2 = txt.count('day')
+word_count3 = txt.count(' ')
+print(word_count1)
+print(word_count2)
+print(word_count3)
