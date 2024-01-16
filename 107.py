ab732ce2eca3b280ed2e72c7efd8a638098ff891
iris-starry <iris_starry_@naver.com>
2024-01-17 02:07:51 +0900

    리스트 합치기(+)

---
+++ 107.py
@@ -0,0 +1,6 @@
+listdata1 = ['a', 'b', 'c', 'd', 'e']
+listdata2 = ['f', 'g', 'h', 'i', 'j']
+listdata3 = listdata1 + listdata2
+listdata4 = listdata2 + listdata1
+print(listdata3)
+print(listdata4)
