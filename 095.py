fbce1fe657ec62c721fddac2921a6be9a4174ecb
iris-starry <iris_starry_@naver.com>
2024-01-17 01:22:01 +0900

    문자열에서 특정 문자(열)를 다른 문자(열)로 바꾸기(replace)

---
+++ 095.py
@@ -0,0 +1,11 @@
+txt = 'My password is 1234'
+ret1 = txt.replace('1','0')
+ret2 = txt.replace('1','python')
+print(ret1)
+print(ret2)
+
+txt = '매일 많은 일들이 일어납니다.'
+ret3 = txt.replace('매일','항상')
+ret4 = ret3.replace('일','사건')
+print(ret3)
+print(ret4)
