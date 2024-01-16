1366a3f580a75bbf5c73da035812bda7beac0ede
iris-starry <iris_starry_@naver.com>
2024-01-17 02:04:57 +0900

    리스트에서 특정 구간에 있는 요소 추출하기

---
+++ 103.py
@@ -0,0 +1,5 @@
+solarsys = ['태양', '수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성']
+rock_planets = solarsys[1:4]
+gas_planets = solarsys[4:]
+print('태양계의 암석형 행성: ', end=''); print(rock_planets)
+print('태양계의 가스형 행성: ', end=''); print(gas_planets)
