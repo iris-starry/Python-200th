60647ee81aff78d19380cbd7019a800b6e2760d8
iris-starry <iris_starry_@naver.com>
2024-01-18 00:15:14 +0900

    리스트 요소가 모두 참인지 확인하기(all, any)

---
+++ 122.py
@@ -0,0 +1,9 @@
+listdata1 = [0, 1, 2, 3, 4]
+listdata2 = [True, True, True]
+listdata3 = ['', [], (), {}, None, False]
+print(all(listdata1))
+print(any(listdata1))
+print(all(listdata2))
+print(any(listdata2))
+print(all(listdata3))
+print(any(listdata3))
