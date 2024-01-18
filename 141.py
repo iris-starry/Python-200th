ee9a2f0ed0757a8cb36b61321833732a603c2ef8
iris-starry <iris_starry_@naver.com>
2024-01-18 22:04:53 +0900

    텍스트 파일에 한줄씩 쓰기(wirtelines)

---
+++ 141.py
@@ -0,0 +1,13 @@
+count = 1
+data = [ ] 
+print('파일에 내용을 저장하려면 내용을 입력하지 말고 [Enter]를 누르세요')
+while True:
+	text = input('[%d] 파일에 저장할 내용을 입력하세요: ' %count)
+	if text == '':
+		break
+	data.append(text+'\n')
+	count += 1
+
+f = open('mydata.txt', 'w')
+f.writelines(data)
+c.close()
